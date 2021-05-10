ed34d40dbe006c074b1d0cf672f1a55cafcdb82b
Sergii Sichynskyi <s.sichynskyi@gmail.com>
2021-05-11 01:12:55 +0200

    add environment configuration file

---
+++ utils/env_config.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import re
+import os
+import yaml
+
+from pathlib import Path
+
+CONFIG_PATH = 'config'
+"""path to config files folder from project root"""
+
+
+class ConfigParser:
+    """Extracts the data from the configuration file given"""
+    def __new__(cls, path):
+        with open(path, 'r') as f:
+            contents = f.read()
+            return yaml.safe_load(contents)
+
+
+_path_to_config = Path(__file__).parent.parent.joinpath(CONFIG_PATH, 'hosts.yaml')
+config = ConfigParser(_path_to_config)
